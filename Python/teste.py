0342f83db112c7f08e1890701852a40b3ac14f0b
Igor Barroso Almeida <igorba867@gmail.com>
2022-12-14 21:26:12 -0300

    date example

--- Python/teste.py
+++ Python/teste.py
@@ -1,1 +1,7 @@
-print("Ola mundo", "estou", "começando", "a", "aprender python", sep="__", end="!\n")
+# print("Ola mundo", "estou", "começando", "a", "aprender python", sep="__", end="!\n")
+
+dia = 15
+mes = 10
+ano = 2015
+
+print(dia, mes, ano, sep="/")
