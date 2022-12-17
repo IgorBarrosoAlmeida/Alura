51816aeceec45379e04d25172b10cb652f68ad1c
Igor Barroso Almeida <igorba867@gmail.com>
2022-12-17 15:29:35 -0300

    Modified conditions

--- Python e POO/adivinhacao.py
+++ Python e POO/adivinhacao.py
@@ -14,9 +14,15 @@ num_secreto = 42
 
 num = int(input("Digite um numero: "))
 
-if(num == num_secreto):
+acertou = (num == num_secreto)
+menor   = (num < num_secreto)
+maior   = (num > num_secreto)
+
+if(acertou):
     print("Você acertou :)")
-else:
-    print("Você errou ;-;")
+elif(menor):
+    print("Você chutou um numero menor")
+elif(maior):
+    print("Você chutou um numero maior")
 
 print("------------FIM DE JOGO------------")
