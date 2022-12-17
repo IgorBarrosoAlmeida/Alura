c8f8a127b17908c6f12c8d36f99460e73867a82c
Igor Barroso Almeida <igorba867@gmail.com>
2022-12-17 15:49:47 -0300

    add rodadas

--- Python e POO/adivinhacao.py
+++ Python e POO/adivinhacao.py
@@ -11,18 +11,24 @@ print("Bem vindo ao jogo de adivinhação!!")
 print("-----------------------------------")
 
 num_secreto = 42
+max_tentativas = 3
+rodada = 1
 
-num = int(input("Digite um numero: "))
+while (rodada <= max_tentativas):
+    print(f"Tentativa {rodada} de {max_tentativas}")
+    num = int(input("Digite um numero: "))
 
-acertou = (num == num_secreto)
-menor   = (num < num_secreto)
-maior   = (num > num_secreto)
+    acertou = (num == num_secreto)
+    menor   = (num < num_secreto)
+    maior   = (num > num_secreto)
 
-if(acertou):
-    print("Você acertou :)")
-elif(menor):
-    print("Você chutou um numero menor")
-elif(maior):
-    print("Você chutou um numero maior")
+    if(acertou):
+        print("Você acertou :)")
+        break
+    elif(menor):
+        print("Você chutou um numero menor")
+    elif(maior):
+        print("Você chutou um numero maior")
+    rodada += 1
 
 print("------------FIM DE JOGO------------")
